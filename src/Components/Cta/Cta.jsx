a6bb7e6de8900bff5927a3160c3fd84bdd043f7b
Aboo00d <abdsadalden2001@gmail.com>
2023-12-17 15:46:47 +0200

    Setting The Mai Folder And Creating The Main Componentes

---
+++ src/Components/Cta/Cta.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Cta.css";
+const Cta = () => {
+  return <div>Cta</div>;
+};
+
+export default Cta;
---
+++ src/Containers/WhatGPT3/WhatGPT3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./WhatGP3.css";
+const WhatGPT3 = () => {
+  return <div>WhatGPT3</div>;
+};
+
+export default WhatGPT3;
