ff61509ecb395f1997ebc00fe3f58df9a30b8f0f
Aboo00d <abdsadalden2001@gmail.com>
2023-12-18 20:15:32 +0200

    Creating The Blog Section

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Article.css";
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imageUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container__article">
+      <div className="gpt3__blog-container__article-image">
+        <img src={imageUrl} alt="Blog Image" />
+      </div>
+      <div className="gpt3__blog-container__article-content">
+        <p>{date}</p>
+        <h3>{title}</h3>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/Containers/Blog/Blog.jsx
+++ src/Containers/Blog/Blog.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 import "./Blog.css";
+import { Article } from "../../Components";
+import { blog01, blog02, blog03, blog04, blog05 } from "./import";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          a Lot Is Happening We Are Blogging About
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container__groupA">
+          <Article
+            imageUrl={blog01}
+            date="20/7/2001"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, unde?"
+          />
+        </div>
+        <div className="gpt3__blog-container__groupB">
+          <Article
+            imageUrl={blog02}
+            date="20/7/2001"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, unde?"
+          />
+          <Article
+            imageUrl={blog03}
+            date="20/7/2001"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, unde?"
+          />
+          <Article
+            imageUrl={blog04}
+            date="20/7/2001"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, unde?"
+          />
+          <Article
+            imageUrl={blog05}
+            date="20/7/2001"
+            title="Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, unde?"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
